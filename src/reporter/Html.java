a7833da6357bece8371fc5b933dcea63d61d9126
Daan Wilmer BSc <daan@daanwilmer.nl>
2013-05-16 13:04:33 +0200

    Added basic reporter

---
+++ src/reporter/Html.java
@@ -0,0 +1,81 @@
+package reporter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import model.RequireJsModule;
+import requirejsExtractor.Extractor;
+
+public class Html {
+	private Map<String, RequireJsModule> modules;
+	
+	public static void main(String[] args) {
+		Extractor x = new Extractor("brackets/");
+		RequireJsModule tree = x.extractModules("brackets");
+		
+		Html exporter = new Html();
+		exporter.gatherModules(tree);
+		exporter.exportModuleDetails();
+	}
+	
+	public Html() {
+		this.modules = new HashMap<String, RequireJsModule>();
+	}
+	
+	public void gatherModules(RequireJsModule module) {
+		RequireJsModule stored = this.modules.get(module.getId());
+		if(stored == null || stored.getDependencies().size() < module.getDependencies().size()) {
+			this.modules.put(module.getId(), module);
+		}
+		for(RequireJsModule dependency : module.getDependencies()) {
+			gatherModules(dependency);
+		}
+	}
+	
+	private void exportModuleDetails() {
+		String filename = "report/report.html";
+		int width = 200;
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(filename));
+			out.write("<!doctype html>");
+			out.write("<html><head><title>Brackets analyzability</title><style type=\"text/css\"></style></head><body><h1>Brackets</h1><ul>");
+			
+			Object[] modules = this.modules.values().toArray();
+			Arrays.sort(modules);
+			for(Object moduleO : modules) {
+				RequireJsModule module = (RequireJsModule) moduleO;
+				int numModules = 0;
+				int numCorrectModules = 0;
+				for(Entry<String, RequireJsModule> entry : module.getNamedDependencies().entrySet()) {
+					numModules++;
+					String moduleId = entry.getValue().getId();
+					String correctName = moduleId.substring(moduleId.lastIndexOf('/') + 1).toLowerCase();
+					if(entry.getKey().toLowerCase().equals(correctName)) {
+						numCorrectModules++;
+					}
+				}
+				int greenWidth = width;
+				if(numModules > 0) {
+					greenWidth = (width * numCorrectModules) / numModules;
+				}
+				out.write("<li>");
+				out.write(module.getId());
+				out.write(": <img src=\"green.gif\" height=10 width=" + greenWidth + ">");
+				out.write("<img src=\"red.gif\" height=10 width=" + (width - greenWidth) + ">");
+				out.write(numCorrectModules + "/" + numModules);
+				out.write("</li>");
+			}
+			
+			out.write("</ul></body></html>");
+			out.flush();
+			out.close();
+		} catch (IOException iox) {
+			iox.printStackTrace();
+		}
+	}
+}
