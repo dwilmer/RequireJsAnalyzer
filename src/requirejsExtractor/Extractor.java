50bcba1e717819b8f370b3b3240f2504c0a443b2
Daan Wilmer BSc <daan@daanwilmer.nl>
2013-05-28 12:20:13 +0200

    Refactored extractor to work with the new version.

--- src/requirejsExtractor/Extractor.java
+++ src/requirejsExtractor/Extractor.java
@@ -5,60 +5,53 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import model.RequireJsModule;
 import analyzer.DependencyAnalyzer;
 import analyzer.FileAnalyzer;
 import analyzer.FunctionCallAnalyzer;
 import analyzer.VariableDefinitionAnalyzer;
-
-import model.FunctionReference;
-import model.RequireJsModule;
-import model.Tupel;
 
 public class Extractor {
 	private Set<String> traversedFiles;
 	private String baseFolder;
 	
 	public static void main(String[] args) {
-		testFunctionCalls(args);
+//		testFunctionCalls(args);
 	}
 	
-	private static void testFunctionCalls(String[] args) {
-		Extractor x = new Extractor("brackets/");
-		ReadResults results = x.readModule("brackets");
-		
-		System.out.println("Required:");
-		for(Tupel<String, String> required : results.dependencies) {
-			System.out.println(" - " + required.a);
-		}
-		System.out.println("Defined:");
-		for(Tupel<Integer, String> required : results.definitions) {
-			System.out.println(" - " + required.b);
-		}
-		System.out.println("Called:");
-		for(Tupel<Integer,Tupel<String, String>> functionCall: results.functionCalls) {
-			System.out.println(" - " + functionCall.b.a + "." + functionCall.b.b + "()");
-		}
-	}
+//	private static void testFunctionCalls(String[] args) {
+//		Extractor x = new Extractor("brackets/");
+//		ReadResults results = x.readModule("brackets");
+//		
+//		System.out.println("Required:");
+//		for(Tupel<String, String> required : results.dependencies) {
+//			System.out.println(" - " + required.a);
+//		}
+//		System.out.println("Defined:");
+//		for(Tupel<Integer, String> required : results.definitions) {
+//			System.out.println(" - " + required.b);
+//		}
+//		System.out.println("Called:");
+//		for(Tupel<Integer,Tupel<String, String>> functionCall: results.functionCalls) {
+//			System.out.println(" - " + functionCall.b.a + "." + functionCall.b.b + "()");
+//		}
+//	}
 	
-	private static void getAndPrintDependencies(String[] args) {
-		String filename = args[0];
-		String baseFolder = "";
-		int slashIndex = filename.lastIndexOf('/'); 
-		if(slashIndex >= 0) {
-			baseFolder = filename.substring(0, slashIndex + 1);
-			filename = filename.substring(slashIndex + 1);
-		}
-		Extractor extract = new Extractor(baseFolder);
-		RequireJsModule tree = extract.extractModules(filename);
-		System.out.println(tree.getTreeString(0));
-	}
+//	private static void getAndPrintDependencies(String[] args) {
+//		String filename = args[0];
+//		String baseFolder = "";
+//		int slashIndex = filename.lastIndexOf('/'); 
+//		if(slashIndex >= 0) {
+//			baseFolder = filename.substring(0, slashIndex + 1);
+//			filename = filename.substring(slashIndex + 1);
+//		}
+//		Extractor extract = new Extractor(baseFolder);
+//		RequireJsModule tree = extract.extractModules(filename);
+//		System.out.println(tree.getTreeString(0));
+//	}
 	
 	public Extractor(String baseFolder) {
 		this.traversedFiles = new HashSet<String>();
@@ -66,47 +59,31 @@ public class Extractor {
 	}
 	
 	public RequireJsModule extractModules(String id) {
-		// create module
 		RequireJsModule module = new RequireJsModule(id);
-		
-		// check whether we already traversed it (to avoid recursion)
-		if(traversedFiles.contains(id)) {
-			return module;
-		} else {
-			traversedFiles.add(id);
-		}
-		
-		// read module
-		ReadResults results = readModule(id); //FIXME
-		
-		// extract dependencies
-		for (Tupel<String,String> dependency : results.dependencies) {
-			RequireJsModule depModule = extractModules(dependency.b);
-			if(depModule != null) {
-				if(dependency.a != null) 
-					module.addDependency(dependency.a, depModule);
-				else
-					module.addDependency(depModule);
-			}
-		}
-		
-		// extract function calls
-		for (Tupel<Integer, Tupel<String,String>> functionCall : results.functionCalls) {
-			module.addFunctionCall(new FunctionReference(functionCall.a, functionCall.b.a, functionCall.b.b));
-		}
-		
-		// extract variable definitions
-		for (Tupel <Integer, String> definition : results.definitions) {
-			module.addVariableDefinition(definition.a, definition.b);
-		}
-		
+		this.readModule(module);
 		return module;
 	}
 	
-	private RequireJsModule readModule(String id) {
-		RequireJsModule module = new RequireJsModule(id);
+	public void readModule(RequireJsModule module) {
+		// check whether we already traversed it (to avoid recursion)
+		if(traversedFiles.contains(module.getId())) {
+			return;
+		} else {
+			traversedFiles.add(module.getId());
+		}
+		
+		// read module
+		readModuleContents(module);
+		
+		// read dependencies
+		for (RequireJsModule dependency : module.getDependencies()) {
+			readModule(dependency);
+		}
+	}
+	
+	private RequireJsModule readModuleContents(RequireJsModule module) {
 		try {
-			String filename = this.baseFolder + id + ".js";
+			String filename = this.baseFolder + module.getId() + ".js";
 			BufferedReader reader = new BufferedReader(new FileReader(filename));
 			List<FileAnalyzer> analyzers = this.getFileAnalyzers();
 			String line = null;
